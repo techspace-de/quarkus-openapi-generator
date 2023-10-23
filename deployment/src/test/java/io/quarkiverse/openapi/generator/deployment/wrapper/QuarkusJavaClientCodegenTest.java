caac29de1b2e0dd6f1413ef79e8fb94708c9ad34
Christian Berger <chberger@users.noreply.github.com>
2023-10-23 17:43:37 +0200

    test: generation of constant names

--- deployment/src/test/java/io/quarkiverse/openapi/generator/deployment/wrapper/QuarkusJavaClientCodegenTest.java
+++ deployment/src/test/java/io/quarkiverse/openapi/generator/deployment/wrapper/QuarkusJavaClientCodegenTest.java
@@ -16,7 +16,9 @@ class QuarkusJavaClientCodegenTest {
             "123456,String,_123456",
             "quarkus_resources,String,QUARKUS_RESOURCES",
             "123456,Integer,NUMBER_123456", // old behavior
-            "123+123,Long,NUMBER_123PLUS_123" // old behavior
+            "123+123,Long,NUMBER_123PLUS_123", // old behavior
+            "M123,String,M123",
+            "MA456,String,MA456"
     })
     void toEnumVarName(String value, String dataType, String expectedVarName) {
 
